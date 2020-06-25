688d0c854b1c595deaacc0731b19ca667350e22d
jideabdqudus <jideabdqudus@gmail.com>
2020-06-25 13:25:37 +0100

    Added Commit

--- src/components/TagCard/index.jsx
+++ src/components/TagCard/index.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import { Link } from 'gatsby';
-import Config from '../../../config';
-import Utils from '../../utils/pageUtils';
 import style from './tags.module.less';
 
 const TagCard = (props) => {
   const {
     img, name, description, color, href,
   } = props;
-  const tagPage = Config.pages.tag;
   return (
     <div>
       <div className={style.tagCard}>
