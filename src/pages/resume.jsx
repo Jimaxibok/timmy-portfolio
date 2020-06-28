a78d48a3398547739378685e5f917c4a008b256a
jideabdqudus <jideabdqudus@gmail.com>
2020-06-28 23:19:25 +0100

    Added Commit

--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { Document, Page } from 'react-pdf';
 import { Button, Row, Col } from 'antd';
+import { Link } from 'gatsby';
 import SEO from '../components/Seo';
+
 
 export default class Resume extends Component {
   constructor() {
@@ -19,27 +21,17 @@ export default class Resume extends Component {
 
   render() {
     const { pageNumber, numPages } = this.state;
-    const pageToggle = () => {
-      if (pageNumber === 1) {
-        this.setState({ pageNumber: 2 });
-      } else {
-        this.setState({ pageNumber: 1 });
-      }
-      return 1;
-    };
-
     return (
       <div>
         <SEO
           title="Resume"
-          description="My resume consists of my biodata of experience. I'm open to various challenges that come
-          in the way of building various web applications."
+          description="A look at my resume shows my biodata & some experiences."
           path="resume"
         />
         <Document
           file="../webresume.pdf"
           onLoadSuccess={this.onDocumentLoadSuccess}
-          style={{ width: '100%', height: '100%' }}
+          style={{ maxWidth: '100px', maxHeight: '300px' }}
         >
           <Page pageNumber={pageNumber} />
         </Document>
@@ -48,7 +40,11 @@ export default class Resume extends Component {
             <p>{`Page ${pageNumber} of ${numPages}`}</p>
           </Col>
           <Col span={2}>
-            <Button type="primary" onClick={pageToggle}>{pageNumber === 1 ? 'Next Page' : 'Previous Page'}</Button>
+            <Button type="primary">
+              <Link to="/">
+                Go back home
+              </Link>
+            </Button>
           </Col>
         </Row>
       </div>
