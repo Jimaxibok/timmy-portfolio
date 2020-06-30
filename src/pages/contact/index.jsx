3657601a0ea8bdd8a4f1efc22227c6443e4c481e
jideabdqudus <jideabdqudus@gmail.com>
2020-06-30 11:39:26 +0100

    Added Commit

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -10,19 +10,14 @@ const Contact = () => (
     <Layout className="container">
       <SEO
         title="Contact"
-        description="Hello friend jideabdqudus here. You can reach out to me through the contact form on this page."
+        description="Hello friend jideabdqudus here. You can contact me through the contact form on this page."
         path="/contact"
         keywords={['jideabdqudus', 'Jide', 'Qudus', 'Abdul', 'Abdul-Qudus', 'Abdul-Azeez', 'FullStack developer', 'Javascript', 'ReactJS', 'Front-End', 'Software developer', 'NodeJS', 'Designer', 'UI/UX']}
       />
       <Header />
       <SidebarWrapper>
         <div className="marginTopTitle">
-          <h1 className="titleSeparate">
-            Contact
-            <span role="img" aria-label="contact">
-              📞
-            </span>
-          </h1>
+          <h1 className="titleSeparate">Contact</h1>
         </div>
         <Row gutter={[40, 20]}>
           <Col sm={24} md={24} lg={12}>
@@ -32,10 +27,10 @@ const Contact = () => (
               className="widthFull contactImgBorder"
             />
           </Col>
-          <ContactForm />
+          <ContactForm />          
         </Row>
         <footer style={{ textAlign: 'center', padding: '20px' }}>
-          <b>~ Jide Abdul-Qudus Portfolio ~</b>
+            <b>~ Jide Abdul-Qudus Portfolio ~</b>
         </footer>
       </SidebarWrapper>
     </Layout>
